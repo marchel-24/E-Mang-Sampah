642140a23587bae4fca7ec36b3524f5f93fad602
marchel-24 <riojuntak86@gmail.com>
2023-12-11 11:45:43 +0700

    Add Window For MakePost

---
+++ E-Mang Sampah/View/MakePost.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace E_Mang_Sampah.View
+{
+    /// <summary>
+    /// Interaction logic for MakePost.xaml
+    /// </summary>
+    public partial class MakePost : UserControl
+    {
+        private byte[] _imageFile;
+        public MakePost()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUploadPicture_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _imageFile = File.ReadAllBytes(openFileDialog.FileName);
+                tbPostImageURL.IsEnabled = false;
+                tbPostImageURL.Text = "< Uploaded Image >";
+            }
+        }
+
+        private void tbPostContent_TouchEnter(object sender, TouchEventArgs e)
+        {
+
+        }
+    }
+}
